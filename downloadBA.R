7abbfdd6975afa521d492866f3e8927ef832888e
Petr Smirnov <psmirnov2000@gmail.com>
2019-02-11 19:12:26 -0500

    typo fix

--- downloadBA.R
+++ downloadBA.R
@@ -5,4 +5,4 @@ download(file.path(basePath, "hgu219hsensgprobe_20.0.0.tar.gz"), destfile="/pfs/
 download(file.path(basePath, "pd.hgu219.hs.ensg_20.0.0.tar.gz"), destfile="/pfs/out/pd.hgu219.hs.ensg_20.0.0.tar.gz")
 download(file.path(basePath, "hthgu133ahsensgcdf_20.0.0.tar.gz"), destfile="/pfs/out/hthgu133ahsensgcdf_20.0.0.tar.gz")
 download(file.path(basePath, "hthgu133ahsensgprobe_20.0.0.tar.gz"), destfile="/pfs/out/hthgu133ahsensgprobe_20.0.0.tar.gz")
-download(file.path(basePath, "pd.hthgu133.hs.ensg_20.0.0.tar.gz"), destfile="/pfs/out/pd.hthgu133.hs.ensg_20.0.0.tar.gz")
+download(file.path(basePath, "pd.hthgu133a.hs.ensg_20.0.0.tar.gz"), destfile="/pfs/out/pd.hthgu133a.hs.ensg_20.0.0.tar.gz")
